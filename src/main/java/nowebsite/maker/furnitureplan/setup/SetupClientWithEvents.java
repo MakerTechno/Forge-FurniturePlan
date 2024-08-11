a8206b686be2aa2f936fee03a6fded522d4337bb
MakerTechno <102357646+MakerTechno@users.noreply.github.com>
2024-08-11 18:02:38 +0800

    Critical fix net problems

--- src/main/java/nowebsite/maker/furnitureplan/setup/SetupClientWithEvents.java
+++ src/main/java/nowebsite/maker/furnitureplan/setup/SetupClientWithEvents.java
@@ -3,12 +3,8 @@ package nowebsite.maker.furnitureplan.setup;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.neoforge.capabilities.Capabilities;
-import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
-import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
-import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 import nowebsite.maker.furnitureplan.FurniturePlan;
 import nowebsite.maker.furnitureplan.blocks.cookingUtensils.blockentities.renderers.IronPotBlockEntityRenderer;
 import nowebsite.maker.furnitureplan.blocks.singleblockfurniture.blockentities.renderer.CupboardEntityRenderer;
@@ -16,8 +12,6 @@ import nowebsite.maker.furnitureplan.blocks.singleblockfurniture.blockentities.r
 import nowebsite.maker.furnitureplan.blocks.singleblockfurniture.gui.CupboardScreen;
 import nowebsite.maker.furnitureplan.blocks.tableware.blockentities.renderers.FoodPlateBlockEntityRenderer;
 import nowebsite.maker.furnitureplan.blocks.tableware.blockentities.renderers.GlassBBlockEntityRenderer;
-import nowebsite.maker.furnitureplan.networks.CupboardPayloadHandler;
-import nowebsite.maker.furnitureplan.networks.CupboardSyncData;
 import nowebsite.maker.furnitureplan.registry.BlockRegistration;
 import nowebsite.maker.furnitureplan.registry.GUIRegistration;
 import org.jetbrains.annotations.NotNull;
@@ -42,43 +36,5 @@ public class SetupClientWithEvents {
         event.registerBlockEntityRenderer(BlockRegistration.IRON_POT_BLOCK_ENTITY.get(), pContext -> new IronPotBlockEntityRenderer());
         event.registerBlockEntityRenderer(BlockRegistration.CUPBOARD_BLOCK_ENTITY.get(), CupboardEntityRenderer::new);
         event.registerBlockEntityRenderer(BlockRegistration.VASE_B_BLOCK_ENTITY.get(), pContext -> new VaseBBlockEntityRenderer());
-    }
-
-    @SubscribeEvent
-    public static void registerCapabilities(@NotNull RegisterCapabilitiesEvent event) {
-        event.registerBlockEntity(
-                Capabilities.ItemHandler.BLOCK,
-                BlockRegistration.FOOD_PLATE_BLOCK_ENTITY.get(),
-                (blockEntity, direction) -> blockEntity.getLazyItemHandler().get()
-        );
-        event.registerBlockEntity(
-                Capabilities.ItemHandler.BLOCK,
-                BlockRegistration.GLASS_B_BLOCK_ENTITY.get(),
-                (blockEntity, direction) -> blockEntity.getLazyItemHandler().get()
-        );
-        event.registerBlockEntity(
-                Capabilities.ItemHandler.BLOCK,
-                BlockRegistration.IRON_POT_BLOCK_ENTITY.get(),
-                (blockEntity, direction) -> blockEntity.getLazyItemHandler().get()
-        );
-        event.registerBlockEntity(
-                Capabilities.ItemHandler.BLOCK,
-                BlockRegistration.CUPBOARD_BLOCK_ENTITY.get(),
-                (blockEntity, direction) -> blockEntity.createHandler().get()
-        );
-        event.registerBlockEntity(
-                Capabilities.ItemHandler.BLOCK,
-                BlockRegistration.VASE_B_BLOCK_ENTITY.get(),
-                (blockEntity, direction) -> blockEntity.getLazyItemHandler().get()
-        );
-    }
-    @SubscribeEvent
-    public static void registerPayloads(final RegisterPayloadHandlersEvent event) {
-        final PayloadRegistrar registrar = event.registrar("1");
-        registrar.commonToClient(
-                CupboardSyncData.TYPE,
-                CupboardSyncData.STREAM_CODEC,
-                new CupboardPayloadHandler()
-        );
     }
 }
---
+++ src/main/java/nowebsite/maker/furnitureplan/setup/SetupSideEvents.java
@@ -0,0 +1,54 @@
+package nowebsite.maker.furnitureplan.setup;
+
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
+import net.neoforged.neoforge.network.registration.PayloadRegistrar;
+import nowebsite.maker.furnitureplan.FurniturePlan;
+import nowebsite.maker.furnitureplan.networks.CupboardPayloadHandler;
+import nowebsite.maker.furnitureplan.networks.CupboardSyncData;
+import nowebsite.maker.furnitureplan.registry.BlockRegistration;
+import org.jetbrains.annotations.NotNull;
+
+@EventBusSubscriber(modid = FurniturePlan.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+public class SetupSideEvents {
+    @SubscribeEvent
+    public static void registerCapabilities(@NotNull RegisterCapabilitiesEvent event) {
+        event.registerBlockEntity(
+            Capabilities.ItemHandler.BLOCK,
+            BlockRegistration.FOOD_PLATE_BLOCK_ENTITY.get(),
+            (blockEntity, direction) -> blockEntity.getLazyItemHandler().get()
+        );
+        event.registerBlockEntity(
+            Capabilities.ItemHandler.BLOCK,
+            BlockRegistration.GLASS_B_BLOCK_ENTITY.get(),
+            (blockEntity, direction) -> blockEntity.getLazyItemHandler().get()
+        );
+        event.registerBlockEntity(
+            Capabilities.ItemHandler.BLOCK,
+            BlockRegistration.IRON_POT_BLOCK_ENTITY.get(),
+            (blockEntity, direction) -> blockEntity.getLazyItemHandler().get()
+        );
+        event.registerBlockEntity(
+            Capabilities.ItemHandler.BLOCK,
+            BlockRegistration.CUPBOARD_BLOCK_ENTITY.get(),
+            (blockEntity, direction) -> blockEntity.createHandler().get()
+        );
+        event.registerBlockEntity(
+            Capabilities.ItemHandler.BLOCK,
+            BlockRegistration.VASE_B_BLOCK_ENTITY.get(),
+            (blockEntity, direction) -> blockEntity.getLazyItemHandler().get()
+        );
+    }
+    @SubscribeEvent
+    public static void registerPayloads(final @NotNull RegisterPayloadHandlersEvent event) {
+        final PayloadRegistrar registrar = event.registrar("1");
+        registrar.commonToClient(
+            CupboardSyncData.TYPE,
+            CupboardSyncData.STREAM_CODEC,
+            new CupboardPayloadHandler()
+        );
+    }
+}
